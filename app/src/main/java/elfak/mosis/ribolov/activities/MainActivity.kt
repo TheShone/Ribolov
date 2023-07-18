1d2260d1cab23f1210a99c1dbd99851a7078220b
Nenad Pavlovic <nenadp@elfak.rs>
2023-07-18 15:29:48 +0200

    Pocetak

---
+++ app/src/main/java/elfak/mosis/ribolov/activities/MainActivity.kt
@@ -0,0 +1,12 @@
+package elfak.mosis.ribolov.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import elfak.mosis.ribolov.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
